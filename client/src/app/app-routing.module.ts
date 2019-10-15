af818ec6c0e0d406e84cef03568ea74e26724bb3
Paul Friederichsen <floogulinc@gmail.com>
2019-10-15 00:58:21 -0500

    Add test page for testing routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestPageComponent } from './test-page/test-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'test', component: TestPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
