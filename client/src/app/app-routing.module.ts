722d2b6885ea1e91fb9224db74ef1c51627d5add
Paul Friederichsen <floogulinc@gmail.com>
2019-10-29 03:32:50 -0500

    Add new pages to routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TestPageComponent } from './test-page/test-page.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [{path: 'test', component: TestPageComponent}];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'employees', component: EmployeesComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
