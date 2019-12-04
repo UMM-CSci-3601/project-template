80a616108c156b52f35a7b07b6a51e1a1ca4e7cc
Paul Friederichsen <floogulinc@gmail.com>
2019-12-04 01:33:27 -0600

    Improve employee API

--- server/src/main/java/umm3601/employee/Employee.java
+++ server/src/main/java/umm3601/employee/Employee.java
@@ -2,15 +2,23 @@ package umm3601.employee;
 
 import java.time.LocalDate;
 
-import org.bson.types.ObjectId;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import org.mongojack.Id;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 public class Employee {
     
-    //public ObjectId _id;
+    @Id
+    public String id;
+    
     public String name;
     public String emailAddress;
-    public long salary;
+    public long salary = 0;
     //public LocalDate hiredDate;
+    @JsonInclude(Include.NON_NULL)
     public String[] skills;
     
 }
--- server/src/main/java/umm3601/employee/EmployeeController.java
+++ server/src/main/java/umm3601/employee/EmployeeController.java
@@ -1,9 +1,12 @@
 package umm3601.employee;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Updates.*;
@@ -12,7 +15,9 @@ import org.bson.Document;
 import org.bson.types.ObjectId;
 
 import io.javalin.apibuilder.CrudHandler;
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
 import io.javalin.plugin.json.JavalinJson;
 import io.javalin.plugin.openapi.annotations.*;
 
@@ -21,27 +26,29 @@ public class EmployeeController implements CrudHandler {
     static String emailRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
 
 
-    private final MongoCollection<Document> employeeCollection;
+    private MongoCollection<Employee> employeeCollection;
 
-    public EmployeeController(MongoDatabase database) {
-        employeeCollection = database.getCollection("employees");
+    public EmployeeController(MongoCollection<Employee> employeeCollection) {
+        this.employeeCollection = employeeCollection;
     }
 
     @OpenApi(
         summary = "Create employee",
         description = "Create a new employee",
-        responses = @OpenApiResponse(status = "200"),
+        responses = {
+            @OpenApiResponse(status = "200"),
+            @OpenApiResponse(status = "400")
+        },
         requestBody = @OpenApiRequestBody(content = @OpenApiContent(from = Employee.class))
     )
     @Override
     public void create(Context ctx) {
-        ctx.bodyValidator(Employee.class)
-        .check((obj) -> obj.name != null && obj.name != "")
-        .check((obj) -> obj.emailAddress.matches(emailRegex));
+        Employee newEmployee = ctx.bodyValidator(Employee.class)
+            .check((obj) -> obj.name != null && obj.name != "")
+            .check((obj) -> obj.emailAddress.matches(emailRegex))
+            .get();
 
-        Document newDoc = Document.parse(ctx.body());
-
-        employeeCollection.insertOne(newDoc);
+        employeeCollection.insertOne(newEmployee);
     }
 
     @OpenApi(
@@ -59,24 +66,42 @@ public class EmployeeController implements CrudHandler {
     @OpenApi(
         summary = "Get all employees",
         description = "Get an array of all employees",
+        queryParams = {
+            @OpenApiParam(name = "sortby", description = "The attribute to sort by, defaults to name"),
+            @OpenApiParam(name = "sortasc", description = "If true, sorted in ascending order, false descending. If not present, it will not be sorted", type = Boolean.class)
+        },
         responses = @OpenApiResponse(status = "200", 
         content = @OpenApiContent(from = Employee.class, isArray = true))
     )
     @Override
     public void getAll(Context ctx) {
-        ctx.json(employeeCollection.find());
+        String sortBy = ctx.queryParam("sortby", "name"); //Sort by sort query param, default is name
+        Boolean sortAsc = ctx.queryParam("sortasc", Boolean.class).getOrNull();
+
+        ctx.json(employeeCollection.find()
+            .sort(sortAsc == null ? null : sortAsc ? Sorts.ascending(sortBy) : Sorts.descending(sortBy))
+            .projection(Projections.exclude("skills")).into(new ArrayList<>()));
     }
 
     @OpenApi(
         summary = "Get employee",
         description = "Get the record for a single employee",
         pathParams = @OpenApiParam(name = "id"),
-        responses = @OpenApiResponse(status = "200", content = @OpenApiContent(from = Employee.class))
+        responses = {
+            @OpenApiResponse(status = "200", content = @OpenApiContent(from = Employee.class)),
+            @OpenApiResponse(status = "404")
+        }
     )
     @Override
     public void getOne(Context ctx, String resourceId) {
-        ctx.result(employeeCollection.find(eq("_id", new ObjectId(resourceId))).first().toJson());
-
+        Employee employee;
+        //try {
+            employee = employeeCollection.find(eq("_id", new ObjectId(resourceId))).first();
+        //} catch(IllegalArgumentException e) {
+        //    throw new BadRequestResponse();
+        //}
+        if (employee == null) throw new NotFoundResponse();
+        else ctx.json(employee);
     }
 
     @Override
